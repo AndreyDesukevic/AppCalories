87c41eb490c31a321b3981c8c18039c536b7f918
Андрей Десюкевич <70821681+AndreyDesukevic@users.noreply.github.com>
2022-12-16 01:49:59 +0300

    Creating Mosels, Connections DataBase

---
+++ AppCalories.DAL/WebDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AppCalories.Domain.EFStuff.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCalories.DAL
+{
+    public class WebDbContext:DbContext 
+    {
+        public WebDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ AppCalories.Domain/EFStuff/Models/Recipes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCalories.Domain.EFStuff.Models
+{
+    public class Recipes : BaseModel, IOptionsProduct
+    {
+        public decimal Calories { get ; set ; }
+        public decimal Proteins { get; set; }
+        public decimal Carbohydrates { get; set; }
+        public decimal Fats { get; set; }
+    }
+}
